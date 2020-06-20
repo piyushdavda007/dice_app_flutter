f7ca3292aacdfcd0a00afd64848d11c38de24365
piyushdavda007 <pshdavda@gmail.com>
2020-06-20 16:07:51 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/piyushdavda/diceappflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.piyushdavda.diceappflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.blue,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1;
+  int rightDiceNo = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNo = Random().nextInt(6) + 1;
+      rightDiceNo = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNo.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNo.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
